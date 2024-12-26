8e1113f99a7f948743d347194e7c51b9ec218d95
Sreemathi-Muthukrishnan <64546321+Sreemathi-Muthukrishnan@users.noreply.github.com>
2024-12-26 13:22:15 +0530

    converting html into pug

--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const path = require("path");
 const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get("/", (req,res,next) => {
- //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
- res.render('shop');
+router.get("/", (req, res, next) => {
+  //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render("shop", { prods: adminData.products });
 });
 
 module.exports = router;
