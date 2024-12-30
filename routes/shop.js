3c41e90ecc4bcea488da9dd0e27f278c037c3ede
Sreemathi-Muthukrishnan <64546321+Sreemathi-Muthukrishnan@users.noreply.github.com>
2024-12-30 19:16:56 +0530

    adding handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-  res.render("shop", { prods: adminData.products });
+  res.render("shop", { prods: adminData.products,  pageTitle: 'Shop', path: '/', hasProducts: adminData.products.length > 0 });
 });
 
 module.exports = router;
